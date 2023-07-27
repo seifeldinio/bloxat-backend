5610a47ad2ff23448a15316f02687efc410c5741
seifeldin.io <76487753+seifeldinio@users.noreply.github.com>
2023-07-27 11:15:20 +0300

    callback works

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -1,28 +1,26 @@
 const express = require("express");
 
 const router = express.Router();
-const { courses, users } = require("../../models");
-const passport = require("passport");
+const { users, courses, enrollments } = require("../../models");
+// const passport = require("passport");
 
 //OPAY CALLBACK API
 router.post("/opay-callback/:user_id/:course_id", async (req, res) => {
   const user_id = req.params.user_id;
   const course_id = req.params.course_id;
 
-  //   SEND ACKNOWLEDGEMENT TO OPAY WITH STATUS 200 OK
-  res.sendStatus(200);
+  //   //   SEND ACKNOWLEDGEMENT TO OPAY WITH STATUS 200 OK
+  //   res.sendStatus(200);
   try {
     const { ...data } = req.body;
 
-    if (data.payload.status === "SUCCESS") {
+    if (data.status === "SUCCESS") {
       //   console.log("SUCCESS YO!");
 
-      //   GET USER
       const user = await users.findOne({
         where: { id: user_id },
       });
 
-      // GET COURSE
       const course = await courses.findOne({
         where: { id: course_id },
       });
@@ -33,19 +31,21 @@ router.post("/opay-callback/:user_id/:course_id", async (req, res) => {
         course_id: course.id,
       });
 
-      return res.json(enrollmentsReturn);
+      //   SEND ACKNOWLEDGEMENT TO OPAY WITH STATUS 200 OK
+      return res.sendStatus(200);
+      //   return res.json(enrollmentsReturn);
     } else {
       return null;
     }
   } catch (err) {
-    // return res.status(500).json({ message: "NOPE." });
+    return res.status(500).json({ message: err });
   }
 });
 
 //CHECKOUT FUNCTIONS ONLY WITHOUT OPAY'S CALLBACK
 router.post(
   "/checkout/:user_id/:course_id",
-  passport.authenticate("jwt", { session: false }),
+  //   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const user_id = req.params.user_id;
     const course_id = req.params.course_id;
@@ -56,33 +56,23 @@ router.post(
       if (data.status === "SUCCESS") {
         //   console.log("SUCCESS YO!");
 
-        //   GET USER
-        const userReturn = await users.findOne({
-          where: { id: user_id },
-          //DONT SHOW HASH IN THE RESPONSE
-          attributes: { exclude: ["hash"] },
-        });
-
-        //    GET COURSE
-        const courseReturn = await courses.findOne({
-          where: { id: course_id },
-        });
-
         // ENROLL USER
         const enrollmentsReturn = await enrollments.create({
-          user_id: userReturn.id,
-          course_id: courseReturn.id,
+          user_id: user_id,
+          course_id: course_id,
         });
 
-        enrollmentsReturn();
+        return res.json(enrollmentsReturn);
+
+        // enrollmentsReturn();
 
         //   SEND ACKNOWLEDGEMENT TO OPAY WITH STATUS 200 OK
-        return res.sendStatus(200);
+        // return res.sendStatus(200);
       } else {
         return null;
       }
     } catch (err) {
-      return res.status(500).json({ message: "NOPE." });
+      return res.status(500).json({ message: err });
     }
   }
 );
