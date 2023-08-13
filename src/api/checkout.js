cfc17a9b818521dc21d3ce7536f733ea81b76cdd
seifeldin.io <76487753+seifeldinio@users.noreply.github.com>
2023-08-13 19:12:13 +0300

    Test

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -9,42 +9,82 @@ router.post("/opay-callback/:user_id/:course_id", async (req, res) => {
   const user_id = req.params.user_id;
   const course_id = req.params.course_id;
 
-  // Send acknowledgment to OPay with a status of 200 OK
+  //   SEND ACKNOWLEDGEMENT TO OPAY WITH STATUS 200 OK
   res.sendStatus(200);
-
   try {
-    const { payload } = req.body;
+    const { ...data } = req.body;
 
-    if (payload.status === "SUCCESS") {
-      const user = await users.findOne({
+    if (data.payload.status === "SUCCESS") {
+      //   console.log("SUCCESS YO!");
+
+      //   GET USER
+      const userReturn = await users.findOne({
         where: { id: user_id },
+
+        //DONT SHOW HASH IN THE RESPONSE
+        attributes: { exclude: ["hash"] },
       });
 
-      const course = await courses.findOne({
+      // const user = await users.findOne({
+      //   where: { id: user_id },
+      // });
+
+      const courseReturn = await courses.findOne({
         where: { id: course_id },
       });
 
-      if (user && course) {
-        await enrollments.create({
-          user_id: user.id,
-          course_id: course.id,
-        });
-
-        // Respond with a success message and status 200
-        // return res.status(200).json({ message: "Enrollment successful" });
-      } else {
-        // User or course not found
-        return res.status(404).json({ message: "User or course not found" });
-      }
+      await enrollments.create({
+        user_id: userReturn.id,
+        course_id: courseReturn.id,
+      });
     } else {
-      // Respond with a failure message and status 200
-      // return res.status(200).json({ message: "Payment not successful" });
+      return null;
     }
   } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Internal server error" });
+    // return res.status(500).json({ message: "NOPE." });
   }
 });
+
+// router.post("/opay-callback/:user_id/:course_id", async (req, res) => {
+//   const user_id = req.params.user_id;
+//   const course_id = req.params.course_id;
+
+//   // Send acknowledgment to OPay with a status of 200 OK
+//   res.sendStatus(200);
+
+//   try {
+//     const { payload } = req.body;
+
+//     if (payload.status === "SUCCESS") {
+//       const user = await users.findOne({
+//         where: { id: user_id },
+//       });
+
+//       const course = await courses.findOne({
+//         where: { id: course_id },
+//       });
+
+//       if (user && course) {
+//         await enrollments.create({
+//           user_id: user.id,
+//           course_id: course.id,
+//         });
+
+//         // Respond with a success message and status 200
+//         // return res.status(200).json({ message: "Enrollment successful" });
+//       } else {
+//         // User or course not found
+//         return res.status(404).json({ message: "User or course not found" });
+//       }
+//     } else {
+//       // Respond with a failure message and status 200
+//       // return res.status(200).json({ message: "Payment not successful" });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ message: "Internal server error" });
+//   }
+// });
 
 //CHECKOUT FUNCTIONS ONLY WITHOUT OPAY'S CALLBACK
 router.post(
